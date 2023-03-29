69b9b1a3facd7a344cb9d0d3d0d6ebe397dde16a
gustavbarbosaa <gustavoaraujohab@gmail.com>
2023-03-28 22:46:16 -0300

    criacao da categoria

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <headerComponent />
   <homeComponent />
+  <categoryComponent />
 </template>
 
 <script>
 import headerComponent from './components/Header/index.vue';
-import homeComponent from './components/Body/index.vue';
+import homeComponent from './components/Body/Home/index.vue';
+import categoryComponent from './components/Body/Category/index.vue';
 
 export default {
   components: {
     headerComponent,
-    homeComponent
+    homeComponent,
+    categoryComponent,
   }
 }
 </script>
---
+++ src/components/Body/Category/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="category" class="flex flex-col justify-center text-center">
+    <h1 class="font-bold font-roboto text-6xl text-white pt-52 pb-14">Category</h1>
+    <div class="flex justify-between px-60">
+        <div id="card" class="flex flex-col justify-around h-96 w-60">
+            <img src="../../../assets/category1-img.svg" alt="">
+            <h2 class="text-white text-3xl font-semibold font-roboto">Ghost</h2>
+            <p class="font-normal font-roboto text-white text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur. </p>
+        </div>
+        <div id="card" class="flex flex-col justify-around h-96 w-60">
+            <img src="../../../assets/category2-img.svg" alt="">
+            <h2 class="text-white text-3xl font-semibold font-roboto">Witch</h2>
+            <p class="font-normal font-roboto text-white text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur. </p>
+        </div>
+        <div id="card" class="flex flex-col justify-around h-96 w-60">
+            <img src="../../../assets/category3-img.svg" alt="">
+            <h2 class="text-white text-3xl font-semibold font-roboto">Pumpkin</h2>
+            <p class="font-normal font-roboto text-white text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur. </p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: 'categoryComponent',
+}
+</script>
